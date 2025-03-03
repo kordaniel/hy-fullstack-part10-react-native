38c5c7785d027b77fce06e32ede3de3b56f38033
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-03-03 16:44:22 +0200

    Ex. 10.8: The sign-in form

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,5 +1,73 @@
-import Text from './Text';
+import { Pressable, StyleSheet, TextInput, View } from 'react-native';
+import { useFormik } from 'formik';
 
-const SignIn = () => <Text>The sign-in view</Text>;
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    margin: 5,
+    gap: 10,
+    backgroundColor: theme.colors.white,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: theme.colors.textSecondary,
+    borderRadius: 5,
+    padding: 10,
+  },
+  textSubmitBtn: {
+    borderWidth: 1,
+    borderColor: theme.colors.textSecondary,
+    borderRadius: 5,
+    padding: 10,
+  },
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignIn = () => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      console.log('Sign in:', values);
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        placeholder='Username'
+        placeholderTextColor={theme.colors.textSecondary}
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder='Password'
+        placeholderTextColor={theme.colors.textSecondary}
+        secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      <Pressable onPress={formik.handleSubmit}>
+        <Text
+          style={styles.textSubmitBtn}
+          fontSize="subheading"
+          color="white"
+          textAlign="center"
+          bgColor={theme.colors.primary}
+        >
+          Sign In
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
 
 export default SignIn;
